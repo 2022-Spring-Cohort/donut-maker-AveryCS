9ab2057d61be23cf06bcaaea91bf9fe71334e94b
AveryCS <Averydcs@gmail.com>
2022-03-03 15:35:44 -0500

    Got the donut counter button working

---
+++ DonutMaker.js
@@ -0,0 +1,25 @@
+//Will hold methods and fields for everything dealing with donuts.
+//Step 1: need to make methods to add to donut count and retrieve donut count 
+
+export default class DonutMaker{
+constructor(donutCount,donutMultiplier,autoClicker){
+this.donutCount = 0;
+this.donutMultiplier = 0;
+this.autoClicker = 0;
+}
+
+addToDonutCount(){
+    this.donutCount++
+}
+
+
+
+
+
+
+
+
+
+
+
+}
--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const container = (document.querySelector('.container').innerText =
-  'mmmmmmmmm doughnuts.... ahhhhhhh!');
+import DonutMaker from "./DonutMaker.js";
+const donutMaker = new DonutMaker();
+
+// const container = (document.querySelector('.container').innerText =
+//   'mmmmmmmmm doughnuts.... ahhhhhhh!');
+
+
+  donutMaker.addToDonutCount()
+
+  let donutCount = donutMaker.donutCount
+  
+  const addToDonutCount = document.querySelector(".IncreaseDonutCountButton");
+  const donutDisplayCount = document.querySelector(".DonutCountDisplay");
+  addToDonutCount.addEventListener("click", () => {
+    donutMaker.addToDonutCount();
+    donutDisplayCount.innerText = "Donut Count: "+ donutMaker.donutCount;
+
+    
+
+  
+
+})
