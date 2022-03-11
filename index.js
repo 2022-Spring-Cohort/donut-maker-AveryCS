25a1eba55ebfa98cd4c0df0ed665d82b54c0a0bb
AveryCS <Averydcs@gmail.com>
2022-03-11 14:33:05 -0500

    adding stuff for the day

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ setInterval(function(){
   autoClickCostDisplay.innerText = "Auto Clicker Cost: "+ Math.round(donutMaker.autoClickerCost);
   multiplierDisplay.innerText = "Multiplier: "+ Math.round(donutMaker.donutMultiplier);
   multiplierCostDisplay.innerText = "Multiplier Cost: "+ Math.round(donutMaker.donutMultiplierCost);
-  valueofMultiplier.innerText = "Donuts Earned Per Click: "+ Math.pow(1.2, donutMaker.donutMultiplier).toFixed(2);
+  valueofMultiplier.innerText = "Sunflowers Earned Per Click: "+ Math.pow(1.2, donutMaker.donutMultiplier).toFixed(2);
 },150);
 
 setInterval(function(){
